85d1985ea7ffeef7fc8bf6f5ca4d2d1945be16c0
Amine <elouaeramine266@hotmail.com>
2022-04-16 15:19:30 +0100

    tst

--- model.py
+++ model.py
@@ -63,3 +63,14 @@ class UNET(nn.Module):
             x = self.ups[idx + 1](concat_skip)
 
         return self.final_conv(x)
+
+def test():
+    x = torch.randn((3,1,160,160))
+    model = UNET(in_channels=1 , out_channels=1)
+    preds = model(x)
+    print(preds.shape)
+    print(x.shape)
+    assert x.shape == preds.shape
+
+if __name__ == '__main__':
+    test()
